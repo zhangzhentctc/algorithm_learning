6d8322f6fece3ad9c173677cf75dc6d828676fc0
Aaron Zhang <aaron@AarondeMacBook-Pro.local>
2017-05-14 19:11:06 +0800

    Modify Makefile; Add Improved KMP

---
+++ string/kmp/kmp_improve.c
@@ -0,0 +1,79 @@
+#include<stdio.h>
+#include<string.h>
+
+int calcNext(char * p, int next[])
+{
+    int nlen = strlen(p);
+    next[0] = -1;
+    int k = -1;
+    int j = 0;
+
+    while ( j < nlen - 1)
+    {
+        if ( k == -1 || p[j] == p[k] )
+        {
+            ++k;
+            ++j;
+            if( p[j] == p[k] )
+                next[j] = next[k];
+            else
+                next[j] = k;
+        }
+        else
+        {
+            k = next[k];
+        }
+    }
+
+    return 1;
+
+}
+
+int kmp(char * g_s, char * g_pattern, int g_next[])
+{
+    int ans = -1;
+    int i = 0;
+    int j = 0;
+    int s_len = strlen(g_s);
+    int pattern_len = strlen(g_pattern);
+    while ( i < s_len )
+    {
+        if ( j == -1 || g_s[i] == g_pattern[j])
+        {
+            ++i;
+            ++j;
+        }
+        else
+            j = g_next[j];
+        if( j == pattern_len )
+        {
+            ans = i - pattern_len;
+            break;
+        }
+    }
+    return ans;
+
+}
+
+int main()
+{
+    char * g_s = "I'm a good boy";
+    char * g_pattern = "boy";
+    int ret = 0;
+    int next[4];
+    if(!calcNext(g_s, next))
+    {
+        printf("Calculate Next Array Error\n");
+    }
+
+    ret = kmp(g_s, g_pattern, next);
+    if(ret == -1)
+    {
+        printf("Not found\n");
+    }
+    else
+        printf("Result:%d\n", ret);
+    return 1;
+}
+
+
